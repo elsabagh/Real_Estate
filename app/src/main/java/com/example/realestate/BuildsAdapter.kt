cfc5b3cb1528e208f0e6249da1b4fa2fc9bf9f40
elsabagh <elsabagha025@gmail.com>
2022-12-13 17:09:43 +0200

    update home page

--- app/src/main/java/com/example/realestate/BuildsAdapter.kt
+++ app/src/main/java/com/example/realestate/BuildsAdapter.kt
@@ -7,13 +7,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class BuildsAdapter(private val buildsList:ArrayList<Data_builds>)
+class BuildsAdapter(private var buildsList:ArrayList<Data_builds>)
     :RecyclerView.Adapter<BuildsAdapter.BuildViewHolder>() {
 
         class BuildViewHolder(itemsView: View) : RecyclerView.ViewHolder(itemsView){
             val imageView : ImageView = itemsView.findViewById(R.id.image_build)
             val textView: TextView = itemsView.findViewById(R.id.textView3)
         }
+
+    fun setFilteredList(buildsList: ArrayList<Data_builds>){
+        this.buildsList = buildsList
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuildViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.builds_item , parent , false)
--- app/src/main/java/com/example/realestate/Home_Fragment.kt
+++ app/src/main/java/com/example/realestate/Home_Fragment.kt
@@ -1,21 +1,24 @@
 package com.example.realestate
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.realestate.databinding.FragmentHomeBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Home_Fragment : Fragment() {
 
 lateinit var binding: FragmentHomeBinding
 
-    private lateinit var recyclerView: RecyclerView
     private lateinit var buildList: ArrayList<Data_builds>
     private lateinit var buildsAdapter: BuildsAdapter
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,18 +36,51 @@ lateinit var binding: FragmentHomeBinding
         val layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
         binding.recBuilds.layoutManager = layoutManager
 
-
         buildList = ArrayList()
-
-        buildList.add(Data_builds(R.drawable.build1, "Om Sree heights"))
-        buildList.add(Data_builds(R.drawable.build1, "Om Sree heights"))
-        buildList.add(Data_builds(R.drawable.build1, "Om Sree heights"))
-        buildList.add(Data_builds(R.drawable.build1, "Om Sree heights"))
-        buildList.add(Data_builds(R.drawable.build1, "Om Sree heights"))
 
         buildsAdapter = BuildsAdapter(buildList)
         binding.recBuilds.adapter = buildsAdapter
+        addDataToList()
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+        })
+    }
 
 
+    private fun filterList(query: String?) {
+
+        if (query != null) {
+            val filteredList = ArrayList<Data_builds>()
+            for (i in buildList) {
+                if (i.name.lowercase(Locale.ROOT).contains(query)) {
+                    filteredList.add(i)
+                }
+            }
+
+            if (filteredList.isEmpty()) {
+                Toast.makeText(context, "No Data found", Toast.LENGTH_SHORT).show()
+            } else {
+                buildsAdapter.setFilteredList(filteredList)
+            }
+        }
+    }
+
+    private fun addDataToList() {
+
+        buildList.add(Data_builds(R.drawable.build1, "Om Sree heights"))
+        buildList.add(Data_builds(R.drawable.build1, "Om2 Sree heights"))
+        buildList.add(Data_builds(R.drawable.build1, "Om3 Sree heights"))
+        buildList.add(Data_builds(R.drawable.build1, "Om4 Sree heights"))
+        buildList.add(Data_builds(R.drawable.build1, "Om5 Sree heights"))
+
+        buildsAdapter.notifyDataSetChanged()
     }
 }
